e74e2b27c0b04b1120adce0dc194a9f02d86d2d9
Nora Blomaard <n.blomaard@gmail.com>
2020-03-06 11:31:48 +0100

    Add options to home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import Car from "../assets/car.svg";
+import Find from "../assets/find.svg"
 
 const HomeScreen = () => {
  return (
-   <div>
-    <Button variant="primary" size="lg" block>Kies een merk</Button>
-    <Button variant="primary" size="lg" block>Ontdek een auto</Button>
+   <div className="home-items">
+    <Link to="/">
+      <Button className="choice-button" variant="primary" size="lg" block>
+        Kies een merk
+        <img className="button-image" src={Car}/>
+      </Button>
+    </Link>
+    <Link to="/ChoiceWizard">
+    <Button variant="primary" size="lg" block>
+      Ontdek een auto
+      <img className="button-image" src={Find}/>
+    </Button>
+    </Link>
     <a href="">Sla over</a>
   </div>
  )
